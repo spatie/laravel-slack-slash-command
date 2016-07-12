37b99727bcf30da08cb9cf7bbbe2518417f1a124
freek <freek@spatie.be>
2016-07-12 23:42:21 +0200

    wip

---
+++ tests/SlashCommandControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Spatie\SlashCommand\Test;
+
+class SlashCommandControllerTest extends TestCase
+{
+    /** @test */
+    public function it_can()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Spatie\SlashCommand\Test;
+
+use Illuminate\Contracts\Console\Kernel;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Spatie\SlashCommand\SlashCommandServiceProvider::class,
+        ];
+    }
+
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('app.key', '6rE9Nz59bGRbeMATftriyQjrpF7DcOQm');
+    }
+}
