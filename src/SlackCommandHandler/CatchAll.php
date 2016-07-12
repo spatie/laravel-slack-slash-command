a99255b1883963eb65bf13be0a7ef36d9366f10f
freek <freek@spatie.be>
2016-07-12 23:55:09 +0200

    wip

--- src/SlackCommandHandler/CatchAll.php
+++ src/SlackCommandHandler/CatchAll.php
@@ -9,7 +9,7 @@ class CatchAll extends BaseHandler
 {
     public function handleCurrentRequest(): SlashCommandResponse
     {
-        $this->dispatch(new TestJob());
+        //$this->dispatch(new TestJob());
 
         return $this->respondToSlack("Received this message `{$this->slashCommandData->text}`");
     }
