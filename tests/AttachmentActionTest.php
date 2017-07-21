9184dbe08cc327d44d6325553034d794cf99a7f8
Hunter Skrasek <hunterskrasek@me.com>
2017-07-20 21:15:28 -0500

    Add a test for setting an actions style
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- tests/AttachmentActionTest.php
+++ tests/AttachmentActionTest.php
@@ -19,4 +19,18 @@ class AttachmentActionTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('button', $action['type']);
         $this->assertSame('value', $action['value']);
     }
+
+    /** @test */
+    public function test_it_can_set_a_style()
+    {
+        $attachmentAction = AttachmentAction::create('action', 'an action', 'button')
+                                            ->setStyle(AttachmentAction::STYLE_PRIMARY);
+
+        $action = $attachmentAction->toArray();
+
+        $this->assertSame('action', $action['name']);
+        $this->assertSame('an action', $action['text']);
+        $this->assertSame('button', $action['type']);
+        $this->assertSame('primary', $action['style']);
+    }
 }
