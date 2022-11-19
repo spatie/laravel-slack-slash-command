fe83e2cf89a419db3e1e7bca122f09209ace47c2
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-19 19:10:02 +0300

    refactor tests

--- tests/Handlers/CatchAllTest.php
+++ tests/Handlers/CatchAllTest.php
@@ -2,25 +2,18 @@
 
 namespace Spatie\SlashCommand\Test\Handlers;
 
-use Spatie\SlashCommand\Test\TestCase;
+it('can catch a request', function () {
+    $command = '/my command';
+    $text = 'some text';
 
-class CatchAllTest extends TestCase
-{
-    /** @test */
-    public function it_can_catch_a_request()
-    {
-        $command = '/my command';
-        $text = 'some text';
+    $response = $this->call('POST', 'test-url', [
+        'token' => 'test-token',
+        'command' => $command,
+        'text' => $text,
+    ]);
 
-        $response = $this->call('POST', 'test-url', [
-            'token' => 'test-token',
-            'command' => $command,
-            'text' => $text,
-        ]);
-
-        $response->assertSuccessful();
-        $response->assertJsonFragment([
-            'text' => "I did not recognize this command: `{$command} {$text}`",
-        ]);
-    }
-}
+    $response->assertSuccessful();
+    $response->assertJsonFragment([
+        'text' => "I did not recognize this command: `{$command} {$text}`",
+    ]);
+});
--- tests/Handlers/SignaturePartsTest.php
+++ tests/Handlers/SignaturePartsTest.php
@@ -4,49 +4,29 @@ namespace Spatie\SlashCommand\Test\Handlers;
 
 use Spatie\SlashCommand\Exceptions\InvalidSignature;
 use Spatie\SlashCommand\Handlers\SignatureParts;
-use Spatie\SlashCommand\Test\TestCase;
 
-class SignaturePartsTest extends TestCase
-{
-    /** @var \Spatie\SlashCommand\Handlers\SignatureParts */
-    protected $signatureParts;
+beforeEach(function () {
+    $this->signatureParts = new SignatureParts('/commandName handlerName {argument} {--option}');
+});
 
-    public function setUp(): void
-    {
-        $this->signatureParts = new SignatureParts('/commandName handlerName {argument} {--option}');
+it('can determine the commandName', function () {
+    $this->assertSame('commandName', $this->signatureParts->getSlashCommandName());
+});
 
-        parent::setUp();
-    }
+it('can determine the handlerName', function () {
+    $this->assertSame('handlerName', $this->signatureParts->getHandlerName());
+});
 
-    /** @test */
-    public function it_can_determine_the_commandName()
-    {
-        $this->assertSame('commandName', $this->signatureParts->getSlashCommandName());
-    }
+it('can determine the signature without the commandName', function () {
+    $this->assertSame('handlerName {argument} {--option}', $this->signatureParts->getSignatureWithoutCommandName());
+});
 
-    /** @test */
-    public function it_can_determine_the_handlerName()
-    {
-        $this->assertSame('handlerName', $this->signatureParts->getHandlerName());
-    }
+it('can determine the arguments and options', function () {
+    $this->assertSame('{argument} {--option}', $this->signatureParts->getArgumentsAndOptions());
+});
 
-    /** @test */
-    public function it_can_determine_the_signature_without_the_commandName()
-    {
-        $this->assertSame('handlerName {argument} {--option}', $this->signatureParts->getSignatureWithoutCommandName());
-    }
+it('will throw an exception if a signature does not contain a space', function () {
+    $this->expectException(InvalidSignature::class);
 
-    /** @test */
-    public function it_can_determine_the_arguments_and_options()
-    {
-        $this->assertSame('{argument} {--option}', $this->signatureParts->getArgumentsAndOptions());
-    }
-
-    /** @test */
-    public function it_will_throw_an_exception_if_a_signature_does_not_contain_a_space()
-    {
-        $this->expectException(InvalidSignature::class);
-
-        new SignatureParts('commandName');
-    }
-}
+    new SignatureParts('commandName');
+});
