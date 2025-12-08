09c94616e4f2d22cb17ea590f05ab4a804c43b43
Lode Claassen <lode@alsvanzelf.nl>
2025-12-08 21:28:20 +0100

    add some basic tests for the base handler

---
+++ tests/Handlers/BaseHandlerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Spatie\SlashCommand\Test\Handlers;
+
+use Spatie\SlashCommand\Exceptions\InvalidHandler;
+use Spatie\SlashCommand\Handlers\BaseHandler;
+use Spatie\SlashCommand\Request;
+use Spatie\SlashCommand\Response;
+
+function getPostParametersForBaseHandler(array $mergeVariables = []): array
+{
+    return array_merge([
+        'token' => 'test-token',
+        'team_id' => 'T123',
+        'team_domain' => 'Company',
+        'channel_id' => 'C123',
+        'channel_name' => 'General',
+        'user_id' => 'U123',
+        'user_name' => 'Bob',
+        'command' => '/commandName',
+        'text' => 'handlerName my-argument --option',
+        'response_url' => 'https://slack.com/respond',
+    ], $mergeVariables);
+}
+
+beforeEach(function () {
+    $illuminateRequest = $this->getIlluminateRequest(getPostParametersForBaseHandler());
+
+    $this->request = Request::createFromIlluminateRequest($illuminateRequest);
+});
+
+it('can handle requests if allowed', function () {
+    $baseHandler = new class($this->request) extends BaseHandler {
+        public function canHandle(Request $request): bool
+        {
+            return true;
+        }
+
+        public function handle(Request $request): Response
+        {
+            return true;
+        }
+    };
+
+    expect($baseHandler->canHandle($this->request))->toBeTrue();
+});
+
+it('cannot handle requests if disallowed', function () {
+    $baseHandler = new class($this->request) extends BaseHandler {
+        public function canHandle(Request $request): bool
+        {
+            return false;
+        }
+
+        public function handle(Request $request): Response
+        {
+            return true;
+        }
+    };
+
+    expect($baseHandler->canHandle($this->request))->toBeFalse();
+});
+
+it('can respond with text', function () {
+    $baseHandler = new class($this->request) extends BaseHandler {
+        public function canHandle(Request $request): bool
+        {
+            return true;
+        }
+
+        public function handle(Request $request): Response
+        {
+            return $this->respondToSlack('Testing 123');
+        }
+    };
+
+    $response = $baseHandler->handle($this->request)->getIlluminateResponse();
+
+    expect($response->getStatusCode())->toBe(200);
+    expect($response->getContent())->toBeJson();
+    expect(json_decode($response->getContent(), associative: true)['text'])->toBe('Testing 123');
+});
